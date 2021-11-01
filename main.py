428808ccc61b91429f13a56b9b12d54d02ff2ae5
BogdanPos18 <bogdanpostolachi18@gmail.com>
2021-11-01 19:56:21 +0200

    console

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from Tests.test_all import run_all_tests
+from UI.command_line_console import command_line
 from UI.console import run_menu
 
 
 def main():
     lista = []
     run_all_tests()
-    run_menu(lista)
+    print("Pentru primul meniu, apasati 1, pentru cel de al doilea, apasati 2")
+    p = input("Alegeti meniul: ")
+    if p == '1':
+        command_line(lista)
+    elif p == '2':
+        run_menu(lista)
 
 
 main()
