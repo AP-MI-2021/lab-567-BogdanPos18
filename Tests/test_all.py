335327e7b42b61af7a82b25c9752fbbf11a9c2e9
BogdanPos18 <bogdanpostolachi18@gmail.com>
2021-11-09 19:07:08 +0200

    Laborator complet

--- Tests/test_all.py
+++ Tests/test_all.py
@@ -1,8 +1,8 @@
 from Tests.test_CRUD import test_adauga_rezervare, test_sterge_rezervare, test_modifica_rezervare, test_get_by_id
+from Tests.test_Undo_Redo import test_undo_redo
 from Tests.test_domain import test_rezervare
 from Tests.test_functionalities import test_mutare_la_clasa_superioara, test_ieftinire_rezervari, \
     test_det_pret_max_per_clasa, test_descrescator_dupa_pret, test_sume_preturi_per_nume
-from UI.test_undo_redo import test_undo_redo
 
 
 def run_all_tests():
